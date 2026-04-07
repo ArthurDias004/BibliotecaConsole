ca43dcc0491e97bd0428389410e56d802c52ab7c
ARTHUR DE OLIVEIRA DIAS <202320482@unifoa.edu.br>
2026-04-07 18:07:18 -0300

    Atualizado

--- Program.cs
+++ Program.cs
@@ -14,18 +14,25 @@ namespace BibliotecaConsole
 
             var livro1 = new Livro("Redes de Computadores", "Autor A");
             var livro2 = new Livro("POO2 na prática", "Autor B");
+            var livro3 = new Livro("Manual dos DevOps", "Autor C");
+            var livro4 = new Livro("Infraestrutura de TI", "Autor D");
 
             var leitor1 = new Leitor("Arthur", "Dias");
             var leitor2 = new Leitor("Cleber", "Dias");
+            var leitor3 = new Leitor("Maria", "Silva");
 
             biblioteca.Livros.Add(livro1);
             biblioteca.Livros.Add(livro2);
+            biblioteca.Livros.Add(livro3);
+            biblioteca.Livros.Add(livro4);
 
             biblioteca.Leitores.Add(leitor1);
             biblioteca.Leitores.Add(leitor2);
+            biblioteca.Leitores.Add(leitor3);
 
             biblioteca.EmprestarLivro("Redes de Computadores", leitor1);
             biblioteca.EmprestarLivro("POO2 na prática", leitor2);
+            biblioteca.EmprestarLivro("Manual dos DevOps", leitor3);
 
             biblioteca.ListarEmprestados();
 
